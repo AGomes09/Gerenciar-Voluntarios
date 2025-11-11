878d4bc33c254fef9da7cbb2630dfbe4dd2d30d8
Adriano <adriano.almeida09@outlook.com>
2025-11-11 01:38:58 -0300

    first commit

---
+++ gerenciarvoluntarios/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import "./header.css";
+import NavBar from "../NavBar/NavBar.jsx";
+function Header() {
+  return <NavBar />;
+}
+
+export default Header;
---
+++ gerenciarvoluntarios/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,6 @@
+import "./navbar.css";
+function NavBar() {
+  return <h1>TesteNAV</h1>;
+}
+
+export default NavBar;
