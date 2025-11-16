5f2c948db881018467be6f71a2f068b57da5e703
Adriano <adriano.almeida09@outlook.com>
2025-11-16 17:30:19 -0300

    feat(Home): Criado a tela de inicio

---
+++ gerenciarvoluntarios/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "src/components/Header/Header.jsx";
+
+function Home() {
+  return (
+    <Header title="Bem-vindo(a) Escolha uma opção ⬇️" background="#000000" />
+  );
+}
+
+export default Home;
