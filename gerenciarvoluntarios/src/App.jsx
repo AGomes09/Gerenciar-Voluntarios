18a67ed3f5c16e175aea953df8c3f5262dc3bf9c
Adriano <adriano.almeida09@outlook.com>
2025-11-16 17:37:53 -0300

    feat(Home): Criada a página de inicio

--- gerenciarvoluntarios/src/App.jsx
+++ gerenciarvoluntarios/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Footer from "src/components/Footer/Footer.jsx";
 import CadastroDeVoluntarios from "./pages/CadastroDeVoluntarios/CadastroDeVoluntarios.jsx";
 import ListaDeVoluntarios from "./pages/ListaDeVoluntarios/ListaDeVoluntarios.jsx";
+import Home from "./pages/Home/Home.jsx";
 import "./app.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Ola</div>} />
+        <Route path="/" element={<Home />} />
         <Route
           path="/cadastrodevoluntarios"
           element={<CadastroDeVoluntarios />}
